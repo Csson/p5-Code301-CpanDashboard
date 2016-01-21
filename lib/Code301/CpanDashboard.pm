5e0fac34d2859e9a0f39937efbad068d275269c7
Erik Carlsson <info@code301.com>
2016-01-21 20:34:54 +0000

    Init

---
+++ lib/Code301/CpanDashboard.pm
@@ -0,0 +1,28 @@
+use 5.14.0;
+use strict;
+use warnings;
+
+package Code301::CpanDashboard;
+
+# VERSION
+# ABSTRACT: Short intro
+
+
+
+1;
+
+__END__
+
+=pod
+
+=head1 SYNOPSIS
+
+    use Code301::CpanDashboard;
+
+=head1 DESCRIPTION
+
+Code301::CpanDashboard is ...
+
+=head1 SEE ALSO
+
+=cut
